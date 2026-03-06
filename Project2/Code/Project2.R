2daecebf0323857016ac76d605c8492d0ff3fda7
DianaF65 <diana.felipe645@gmail.com>
2026-03-06 13:51:03 -0700

    Updating the Project 2 Code folder

---
+++ Project2/Code/Project2.R
@@ -0,0 +1,12 @@
+# Code for Project0
+
+# Libraries
+library(powertools)
+
+# 
+ttest.1samp(N = 400, 
+            delta = 0.3,
+            sd = 2,
+            alpha = 0.5,
+            power = NULL,
+            sides = 2)
