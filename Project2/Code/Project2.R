ef3311b8fb87f736ff63633bc2f38a3b70c429f5
DianaF65 <diana.felipe645@gmail.com>
2026-03-27 10:13:04 -0600

    Progress with aim 2 power analysis

--- Project2/Code/Project2.R
+++ Project2/Code/Project2.R
@@ -4,6 +4,7 @@
 library(powertools)
 library(here)
 library(readr)
+library(dplyr)
 
 # Read in the data
 dat <- read_csv(here("Project2", "Data", "PrelimData.csv"))
@@ -84,19 +85,23 @@ test3
 
 
 # Vector of correlation values
-r <- seq(0.2, 0.80, by = 0.05)
+aim1_r <- seq(0, 0.80, by = 0.02)
 # Square these values for r^squared vals
-r_squared <- r^2
+# r_squared <- r^2
+
+# Adjust this based on what we observe with power
+# Reaches 1 after 0.36
+aim1_r2 <- seq(0, 0.36, by = 0.02)
 
 # Create some more visually appealing r_squared values to report
 # It's in the range of 0.04 to 0.64
 # Perhaps 0.1 to 0.6 by 0.5
-aim1_rsqrd <- seq(0.02, 0.2, by = 0.02)
+# aim1_rsqrd <- seq(0.02, 0.2, by = 0.02)
 
 # Apply the corr.1samp() function to this range of effect sizes
 # What are the possible values of power for the range of correlations with a set
 # sample size of 175?
-aim1_power <- vapply(aim1_rsqrd, function(k) {
+aim1_power <- vapply(aim1_r2, function(k) {
   corr.1samp(
     N = 175,
     rho0 = 0,
@@ -108,7 +113,7 @@ aim1_power <- vapply(aim1_rsqrd, function(k) {
 )
 
 # Create a data frame of the powers
-aim1_power_df <- data.frame(`Effect Size` = aim1_rsqrd,
+aim1_power_df <- data.frame(`Effect Size` = aim1_r2,
                       `Power` = round(aim1_power, 3),
                       check.names = FALSE)
 
@@ -118,6 +123,9 @@ aim1_power_df <- data.frame(`Effect Size` = aim1_rsqrd,
 # Make sure we state that this function is not adjusting for baseline or other
 # covariates
 
+# Transpose data frame for report
+# > a <- data.frame(t(aim1_power_df))
+
 
 ##################################### Aim 2 ####################################
 
@@ -126,20 +134,16 @@ aim1_power_df <- data.frame(`Effect Size` = aim1_rsqrd,
 # Also, Amyloid is measured at baseline. So our interaction will be
 # amyloid(Y/N) x IL-6 (or MCP-1)
 # This will also be a lot easier for interpretation
-
 #1) Although we have proposed using a continuous SUVR variable for amyloid PET, we will
 # examine the utility of incorporating a dichotomous variable for amyloid positivity using recent cutoffs described
 # in the literature (73). 
-
 # TheSUVR threshold of 1.10 had a sensitivity of 97% and a specificity of 100% for 
 # identifying individuals with clinically significant amyloid pathology 
 # (defined as moderate to frequent neuritic plaques by CERAD criteria) (73) 
-
-# (73) reported a rho = 0.79 for correlatino between PET and postmort amyloid
+# (73) reported a rho = 0.79 for correlation between PET and postmort amyloid
 # burden within one year 
 
 # We will use a dichotomous variable of amyloid deposition
-# Create an "interaction" variable for Aim 2
 # Interaction between IL-6 and amyloid (Y/N)
 
 #### From Tasha
@@ -159,29 +163,68 @@ aim1_power_df <- data.frame(`Effect Size` = aim1_rsqrd,
 # We have to get the correlation coefficient for those with no sig. AD
 # And those with sig. AD
 # We have the correlation effects for the main effect of the cytokines/chemokines
-# So we will just have to do it based on that I believe
+# So we will just have to do it based on that I believe..
+# We are going to suppose that the correlation coefficients for the AD
+# group is larger than that of the non AD group
+
+# How to choose n? There are 125 aMCI and 50 HC planned
+# But we also have: Literature indicates that AD pathology may be present in
+# functionally normal adults decades before a diagnosis of MCI or AD (42, 43)
+
+# How much do we expect the correlations between the two groups to differ?
+# Pick values from: 0.1 - 0.5
 
 # Define the new vector of correlations
-aim2_r <- seq(0.25, 0.7, by = 0.05)
+# This is for one group say AD - which we expect to be lower than AD +
+aim2_r <- seq(0.1, 0.3, by = 0.1)
+# Vector of correlations for AD + group
+# Testing for changes of 0.1 - 0.5
+aim2_r2 <- seq(0.2, 0.5, by = 0.1)
 
-# Get the rsqured values
-aim2_rsquared <- aim2_r^2
+# Get the rsqaured values
+# aim2_rsquared <- aim2_r^2
+
+##### WAY 1
+aim2_power <- vapply(aim2_r, function(k) {
+  vapply(aim2_r2, function(i) {
+    corr.2samp(
+      n1 = 175,
+      n.ratio = 1,
+      rho1 = k,
+      rho2 = i,
+      alpha = 0.05,
+      power = NULL,
+      sides = 2
+    )
+  }, numeric(1))
+}, numeric(length(aim2_r2)))
+
+rownames(aim2_power) <- paste0("rho2_", aim2_r2)
+colnames(aim2_power) <- paste0("rho1_", aim2_r)
+  
+#### WAY 2
+# Obtain the diffrent combinations of rho1 and rho2
+grid <- expand.grid(rho1 = aim2_r, rho2 = aim2_r2)
 
 # Calculate power
-aim2_power <- vapply(aim2_rsquared, function(k) {
-  corr.1samp(
-    N = 175,
-    rho0 = k,
-    rhoA = k,
+grid$power <- mapply(function(k, i) {
+  corr.2samp(
+    n1 = 175,
+    n.ratio = 1,
+    rho1 = k,
+    rho2 = i,
     alpha = 0.05,
     power = NULL,
-    sides = 1)}, 
-  numeric(1)
-)
+    sides = 2
+  )
+}, grid$rho1, grid$rho2)
 
-# Power calculations for 
-# Create a data frame of the powers
-aim1_power_df <- data.frame(`Effect Size` = aim2_rsquared,
-                            `Power` = round(aim2_rsquared, 3),
-                            check.names = FALSE)
+# Add a column with differences
+grid$change <- (grid$rho1 - grid$rho2)
+
+# Arrange by change
+grid <- grid %>% 
+  arrange(change)
+
+
 
