be524b978ba419ab1f7d87d6b3dce6874ff8102c
DianaF65 <diana.felipe645@gmail.com>
2026-04-01 14:42:43 -0600

    Exploring data

--- Project3/Code/Project3.R
+++ Project3/Code/Project3.R
@@ -1,1 +1,51 @@
-# Code for Project0
+################################################################################
+###                                  Project 3                               ###
+################################################################################
+
+# Load libraries
+library(readr)
+library(here)
+library(dplyr)
+library(ggplot2)
+library(naniar)
+library(see)
+library(pillar)
+
+# Read in dataset
+frame_data <- read_csv(here("Project3", "Data", "frmgham2.csv"))
+
+################################################################################
+###                        Data Exploration and Cleaning                     ###
+################################################################################
+
+#### First glimpse of the data
+glimpse(frame_data)
+
+# Summary of vars in data
+summary(frame_data)
+
+#
+
+### Subset data to 10 years
+# 10 years is 3600 days
+ten_yrs <- frame_data %>% 
+  dplyr::filter(TIMESTRK <= 3600)
+
+# Subjects that had a stroke at time 0
+stroke_befoe <- ten_yrs %>% 
+  group_by(RANDID) %>% 
+  filter(STROKE == 1 & TIMESTRK == 0) %>% 
+  # filter(STROKE == 1 & TIME == 0) %>% 
+  reframe(RANDID, STROKE, TIMESTRK, TIME,
+          # Create new variables
+          STROKE10 = 1,
+         TIMESTRK10 = 0 ) 
+# There were 32 subjects that had strokes before that we will remove
+
+# Create new STROKE and STRKE10 years relative to 10 years
+
+
+
+
+
+
