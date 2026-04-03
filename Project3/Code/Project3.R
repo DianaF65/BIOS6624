bd663481cb0101cd865ce663cf9607b805e2d6f9
DianaF65 <diana.felipe645@gmail.com>
2026-04-03 11:29:41 -0600

    initial data exploration

--- Project3/Code/Project3.R
+++ Project3/Code/Project3.R
@@ -24,15 +24,11 @@ glimpse(frame_data)
 # Summary of vars in data
 summary(frame_data)
 
-#
+# Interest is time to stroke within first 10 years of study
 
-### Subset data to 10 years
-# 10 years is 3600 days
-ten_yrs <- frame_data %>% 
-  dplyr::filter(TIMESTRK <= 3600)
 
 # Subjects that had a stroke at time 0
-stroke_befoe <- ten_yrs %>% 
+stroke_before <- frame_data %>% 
   group_by(RANDID) %>% 
   filter(STROKE == 1 & TIMESTRK == 0) %>% 
   # filter(STROKE == 1 & TIME == 0) %>% 
@@ -40,9 +36,29 @@ stroke_befoe <- ten_yrs %>%
           # Create new variables
           STROKE10 = 1,
          TIMESTRK10 = 0 ) 
-# There were 32 subjects that had strokes before that we will remove
+# There were 32 subjects that had strokes before 
 
 # Create new STROKE and STRKE10 years relative to 10 years
+# For subjects that had strokes before, we will adjust the time and stroke == 0
+
+# Indicator for whether data is within 10 years
+frame_data <- frame_data %>% 
+  group_by(RANDID) %>% 
+  mutate(within10 = ifelse(TIME > 0 & TIME <= 3600, 1, 0),
+         strokewithin10 = ifelse(TIMESTRK > 0 & TIMESTRK <= 3600, 1, 0)) %>% 
+  relocate(c(within10, strokewithin10), .after = TIME) 
+
+# Create baby data frame to mess around and figure out
+baby <- frame_data %>% 
+  reframe(RANDID, within10, STROKE, PREVSTRK, TIMESTRK, strokewithin10)
+
+# Create a new stroke and time to stroke varible in regard to 10 years
+baby <- baby %>% 
+  mutate(STROKE10 = )
+  
+  
+
+
 
 
 
