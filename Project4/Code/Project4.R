7a7eab3c76b14b76423ff24078a45a56344b82c9
DianaF65 <diana.felipe645@gmail.com>
2026-04-30 12:42:39 -0600

    Progress on background sel sim

--- Project4/Code/Project4.R
+++ Project4/Code/Project4.R
@@ -10,9 +10,12 @@ library(MASS) # AIC/BIC variable selection
 library(olsrr) # Backwards variable selection
 
  
-##### Notes
+################################################################################
+###                                Project Notes                             ###
+################################################################################
+
 ## Correlation among predictors in simulation model: 
-# - Exchaneable with settinggs 0, 0.35, and 0.7
+# - Exchangeable with settings 0, 0.35, and 0.7
 
 ##### Analysis Plan Overview
 ## Use the hrdm package to generate the simulation data
@@ -32,15 +35,19 @@ library(olsrr) # Backwards variable selection
 
 # Simulate the data using the hdrm package
 
+################################################################################
+###                                Backwards Selection                       ###
+################################################################################
+
+
 ################################ Simulation 1 Data #############################
 
 # Define betas
 sim_betas <- c(0.5/3, 1/3, 1.5/3, 2/3, 2.5/3,
-               rep(0, 15))
+               rep(0, 15)) 
 
 ### Functions for model selection techniques
 
-############################### Backwards Selection ############################
 
 # Using Backward variable and correlation.Rmd from BIOS 6624 class as guide
 
@@ -51,64 +58,83 @@ set.seed(646)
 rho_vec <- c(0, 0.35, 0.7)
 
 # Use function from olsrr R package
-sim1 <- function(n = 10000) {
-  # Create list of data frame for different rhos
-  dat_list <- vector("list", length(rho_vec))
-  # Add names to list items
-  names(dat_list) <- paste0("rho_", rho_vec)
-  
+sim1 <- function(N = 10,
+                 # Subjects in each sim
+                 n = 250,
+                 # Betas defined
+                 B = sim_betas,
+                 # Number of vars 1:n_vars to model
+                 n_vars,
+                 # Significance level
+                 alpha = 0.05) {
+  # Get the number of vars
+  if(missing(n_vars)) {n_vars <- length(B)}
+  # List of models 
+  results <- list()
+  # # Add names to list items
+  # names(results) <- paste0("rho_", rho_vec)
   # For range of correlation values
   for(i in seq_along(rho_vec)) {
-    
       # Simulate data
-      dat_list[[i]] <- gen_data(n = 250,
-                                # Number of predictors
-                                p = 20, 
-                                # Non-zero predictors
-                                p1 = 5,
-                                # Vector of non-zero betas
-                                beta = sim_betas,
-                                # Correlation structure
-                                corr = "exchangeable",
-                                # Correlation coefficient
-                                rho = rho_vec[i])
-    }
-  # Extract outcome
-  y <- sim1_data0$y # 
+      sim_dat <- gen_data(n = n,
+                          # Number of predictors
+                          p = 20, 
+                          # Non-zero predictors
+                          p1 = 5,
+                          # Vector of non-zero betas
+                          beta = B,
+                          # Correlation structure
+                          corr = "exchangeable",
+                          # Correlation coefficient
+                          rho = rho_vec[i])
+      # Extract outcome for model
+      y <- sim_dat$y
+      # Extract predictors
+      X <- sim_dat$X
+      # Change colnames
+      colnames(X) <-  seq(1, 20, by = 1)
+      # Create data frame with outcome and predictors
+      model_dat <- data.frame(y, X)
+      # Get all variable names
+      vnames <- names(model_dat)
+      # Fit regression model - exclude intercept
+      results[[i]] <- lm(paste(vnames[1], '~', paste(vnames[2:n_vars],
+                        collapse = '+')),
+                        data = model_dat)
+      # Apply backwards stepwise selection on covariates
+      backwards_sel <- ols_step_backward_p(sim_model) # use include arg here?
+      # use pvalue here?
+  }
   
-  # Extract predictors
-  X <- sim1_data0$X
-  # Change colnames
-  colnames(X) <-  seq(1, 20, by = 1)
-  
-  # Create data frame with outcome and predictors
-  model_dat <- data.frame(y, X)
-  
-  # Fit regression model
-  sim_model <- lm(y ~ ., data = model_dat)
-  
-  # Apply backwards stepwise selection on covariates
-  backwards_sel <- ols_step_backward_p(sim_model,
-                        include = c("X1", "X2", "X3", "X4", "X5"))
-  
-  ### COME Back here
   # Summarize results
+  # Create lists to store info
+  bias <- list()
   
-  # True positive
+  for (i in 1:N) {
+    # True positive - % of time are vars X1-X5 in the model
+    # Get the coefficients
+    
+    # False positive rates
+    
+    # For variables remaining in model: 
+    # Bias - Subtract estimates from defined betas / betas
+    bias[[i]] <- (coef(results[[i]])[, 2:21] - B[1:n_vars])/ B[1:n_vars]
+    # Coverage of the 95% CI
+    
+    # Type I error of vars selected
+    
+    # Type II error of vars selected
+  }
+  # Calculate values
+  bias <- colMeans(do.call('rbind', bias))
   
-  # False positive rates
-  
-  # For variables remaining in model: 
-  # Bias
-  
-  # Coverage of the 95% CI
-  
-  # Type I error of vars selected
-  
-  # Type II error of vars selected
+  # Return results
+  return(data.frame(bias = bias))
 }
 
-#### Checks/Playing around
+
+
+##################### Checks/Playing around
 # Creating simulated data
 set.seed(646)
 sim1_data0 <- gen_data(n = 250,
