4f3c20fb1e3bbc352bac69b9d9de00d60b0f26d0
DianaF65 <diana.felipe645@gmail.com>
2026-05-06 21:03:15 -0600

    Progress on function for varsel and model performance

--- Project4/Code/Project4.R
+++ Project4/Code/Project4.R
@@ -51,230 +51,116 @@ library(glmnet) # LASSO
 
 # 3.) Outermost function that will run through the 6 different possible profiles
 
-############################### Innermost function #############################
+
+################################################################################
+###   Function for: 
+###   Extracting variable selection method results
+###   Summarizing model and variable selection performance
+################################################################################
 
 # This function will extract results from a given model selection and calculate
 # model performance and variable selection performance numbers
+## Calculate metrics for ONE model
 
 model_var_performance <- function(# Resulting model from variable sel method
                                   selection_results,
                                   # Define values for betas
-                                  betas = c(0.5/3, 1/3, 1.5/3, 2/3, 2.5/3,
+                                  true_betas = c(0.5/3, 1/3, 1.5/3, 
+                                                 2/3, 2.5/3,
                                             rep(0, 15)),
+                                  # Define number of variables
+                                  n_vars = 20,
                                   # Define alpha for calculations
                                   alpha = 0.05) {
   
-  # Get the model results
-  
-}
-
-
-
-
-
-
-##############
-
-
-# Define betas
-sim_betas <- c(0.5/3, 1/3, 1.5/3, 2/3, 2.5/3,
-               rep(0, 15)) 
-
-### Functions for model selection techniques
-
-
-# Using Backward variable and correlation.Rmd from BIOS 6624 class as guide
-
-# Set seed
-set.seed(646)
-
-# Specify correlation coefficient values
-rho_vec <- c(0, 0.35, 0.7)
-
-# Simulation function that will iterate N times
-sim1 <- function(N = 10,
-                 # Subjects in each sim
-                 n = 250,
-                 # Betas defined
-                 B = sim_betas,
-                 # Number of vars 1:n_vars to model
-                 n_vars = 20,
-                 # Significance level
-                 alpha = 0.05) {
-  
-  
-  
-  # Lists to store model results and rho values
-  model_results <- list(); rho_results <- c()
-  
-  # Define counter to give each model its own slot because i will not do that
-  counter <- 1
-  
-  # Simulate data N times
-  for (i in 1:N) {
-    
-    # For range of correlation values
-    for (j in seq_along(rho_vec)) {
-      
-      # Simulate data
-      sim_dat <- gen_data(n = n,
-                          # Number of predictors
-                          p = n_vars, 
-                          # Non-zero predictors
-                          p1 = 5,
-                          # Vector of non-zero betas
-                          beta = B,
-                          # Distribution
-                          family = "gaussian",
-                          # Correlation structure
-                          corr = "exchangeable",
-                          # Correlation coefficient
-                          rho = rho_vec[j])
-      
-      # Extract outcome for model
-      y <- sim_dat$y
-      # Extract predictors
-      X <- sim_dat$X
-      # Change colnames
-      colnames(X) <-  paste0("X", 1:n_vars)
-      
-      # Create data frame with outcome and predictors
-      model_dat <- data.frame(y = y, X)
-      # Change colnames of data frame
-      colnames(model_dat) <- c("y", paste0("X", 1:n_vars))
-      
-      # Get all variable names
-      vnames <- names(model_dat)
-      
-      # Fit regression model - exclude intercept
-      full_fit <- lm(as.formula(paste(vnames[1], '~', 
-                                  paste(vnames[2:(n_vars + 1)],
-                                  collapse = '+'))),
-                         data = model_dat)
-      
-      ### Backwards selection
-      backwards_sel <- ols_step_backward_p(full_fit) 
-      # Extract final selected model
-      backwards_fit <- backwards_sel$model
-      # Store the selected variables!
-      model_results[[counter]] <- backwards_fit
-      
-      # Store the rho used for this model fit
-      rho_results[counter] <- rho_vec[j]
-      
-      # Update the counter
-      counter <- counter + 1
-      
-  }
-  
-  # Summarize results
-  # Create lists to store info
-  bias <- list()
-  coverage <- list()
-  tp <- list()
-  fp <- list()
-  typeI_error <- list()
-  typeII_error <- list()
+  # Name the true betas vector for further down
+  names(true_betas) <- paste0("X", 1:n_vars)
   
   # Specify the "true" vars - vars that are associated with y
   true_vars <- paste0("X", 1:5)
   # Specify the null fars - vars not associated with y
   null_vars <- paste0("X", 6:n_vars)
   
-  # Add names to the vector of betas
-  names(B) <- paste0("X", 1:n_vars)
+  # Get the model results
+  varsel_model <- selection_results$model
   
-  for (i in seq_along(model_results)) {
-    # Get the variables that were selected
-    selected_vars <- names(coef(model_results[[i]]))[-1] # Remove intercept
-    
-    # Summarize results
-    ### Bias
-    # Create vector for betas, with NAs for vars not selected
-    bhat <- rep(NA, n_vars)
-    names(bhat) <- paste0("X", 1:n_vars) # Add names to vector
-    
-    # Obtain coefs from model results
-    selected_coefs <- coef(model_results[[i]])[-1] # Remove intercept
-    # Populate bhat vector with the vars from backwards sel
-    bhat[names(selected_coefs)] <- selected_coefs
-    
-    # Calculate bias
-    bias[[i]] <- bhat - B
-    
-    ### Coverage
-    # Get the 95% CI
-    ci <- confint.default(model_results[[i]], level = 1 - alpha)
-    # Remove the intercept from tis
-    ci <- ci[-1, , drop = FALSE]
-    
-    # Create vector for coverage with NAs for vars not selected
-    cover_vec <- rep(NA, n_vars)
-    names(cover_vec) <- paste0("X", 1:n_vars) # Name this vector
-    
-    # Name CI vector with selected vars
-    ci_vars <- rownames(ci)
-    # Populate cover_vec with T/F if 95% CI covered true param value
-    cover_vec[ci_vars] <- ci[, 1] <= B[ci_vars] & B[ci_vars] <= ci[, 2]
-    # Fill coverage list with these results
-    coverage[[i]] <-  cover_vec
-    
-    ### True Positives
-    # Were X1-X5 selected
-    tp_vec <- true_vars %in% selected_vars
-    # Assign names of X1-X5 for this vec
-    names(tp_vec) <- true_vars
-    # Add to list of TPs
-    tp[[i]] <- tp_vec
-    
-    ### False Positives
-    fp_vec <- null_vars %in% selected_vars
-    # Assign names of X6-X20 for this vec
-    names(fp_vec) <- null_vars
-    # Add to list for FPs
-    fp[[i]] <- fp_vec
-    
-    ### Type I Error
-    # Type I error - FP - Times backwards var picked X6-X20
-    typeI_error[[i]] <- fp_vec
-    
-    ### Type II Error
-    # Not selecting the true variables X1-X5
-    typeII_vec <- !(true_vars %in% selected_vars)
-    # Assign names to this
-    names(typeII_vec) <- true_vars
-    # Add to list for type II
-    typeII_error[[i]] <- typeII_vec
-
-  }
+  # Get the variables that were selected
+  selected_vars <- names(coef(varsel_model))[-1] # Remove intercept
   
-  # Convert the lists to data frames
-  bias_df <- do.call(rbind, bias)
-  coverage_df <- do.call(rbind, coverage)
-  tp_df <- do.call(rbind, tp)
-  fp_df <- do.call(rbind, fp)
-  typeI_df <- do.call(rbind, typeI_error)
-  typeII_df <- do.call(rbind, typeII_error)
+  # Summarize results
+  # Create lists to store results
+  
+  
+  ### Bias
+  # Create vector for betas, with NAs for vars not selected
+  bhat <- as.vector(rep(0, n_vars))
+  names(bhat) <- paste0("X", 1:n_vars) # Add names to vector
+  
+  # Populate bhat vector with the vars from selection method - rest as NA
+  bhat[selected_vars] <- as.numeric(varsel_model$coefficients)
+  
+  # Calculate bias
+  bias <- bhat - true_betas
+  
+  ### Coverage
+  # Get the 95% CI
+  ci <- confint.default(varsel_model, level = 1 - alpha)
+  # Remove the intercept from tis
+  ci <- ci[-1, , drop = FALSE]
+  
+  # Create vector for coverage, NAs for vars not selected
+  coverage <- rep(NA, n_vars)
+  names(coverage) <- paste0("X", 1:n_vars) # Name this vector
+  
+  # Name CI vector with selected vars
+  ci_vars <- rownames(ci)
+  # Populate cover_vec with T/F if 95% CI covered true param value
+  coverage[ci_vars] <- ci[, 1] <= true_betas[ci_vars] & # Greater than lower bound
+     true_betas[ci_vars] <= ci[, 2] # Less than upper bound
+  # Fill coverage list with these results
+  # coverage <-  ifelse(is.na(cover_vec), 0, 1)
+  
+  ### True Positives
+  # Vector of length 20
+  tp_vec <- rep(NA, n_vars)
+  names(tp_vec) <- paste0("X", 1:n_vars)
+  # Were X1-X5 selected
+  tp <- true_vars %in% selected_vars
+  tp_vec[true_vars] <- tp
+  
+  ### False Positives
+  fp_vec <- rep(NA, n_vars)
+  names(fp_vec) <- paste0("X", 1:n_vars)
+  # Were X6-X20 selected
+  fp <- null_vars %in% selected_vars
+  fp_vec[null_vars] <- fp
 
+  
+  ### Type I Error
+  # Type I error - FP - Times backwards var picked X6-X20
+  typeI_error <- fp_vec
+  
+  ### Type II Error
+  typeII_vec <- rep(NA, n_vars)
+  names(typeII_vec) <- paste0("X", 1:n_vars)
+  # Not selecting the true variables X1-X5
+  typeII <- !(true_vars %in% selected_vars)
+  typeII_vec[true_vars] <- typeII
+  
   # Create data frame of model performance results
   model_performance_df <- data.frame(
-    variable = paste0("X", 1:n_vars),
-    bias = colMeans(bias_df, na.rm = TRUE),
-    coverage = colMeans(coverage_df, na.rm = TRUE)
+    variable = c(true_vars, null_vars),
+    bias = bias,
+    coverage = coverage
   )
   
   # Create data frame of variable selection results
   selection_df <- data.frame(
-    # Col of variables
     variable = c(true_vars, null_vars),
-    true_positive_rate = c(colMeans(tp_df), 
-                           rep(NA, length(null_vars))),
-    false_positive_rate = c(rep(NA, length(true_vars)), 
-                            colMeans(fp_df)),
-    typeI_error = c(rep(NA, length(true_vars)), 
-                    colMeans(typeI_df)),
-    typeII_error = c(colMeans(typeII_df), 
-                     rep(NA, length(null_vars)))
+    true_positives = tp_vec,
+    false_positives = fp_vec,
+    typeI_error = typeI_error,
+    typeII_error = typeII_vec
   )
   
   # Return list of model performance and var selection results
@@ -283,8 +169,82 @@ sim1 <- function(N = 10,
     selection_performance = selection_df
   ))
   
-  }
 }
+
+# Do a test run
+full_fit <- lm(y ~ ., data = model_dat)
+
+# Backwards selection
+backwards_sel <- ols_step_backward_p(full_fit)
+test_eval <- model_var_performance(backwards_sel)
+model_performance <- test_eval$model_performance
+var_sel_performance <- test_eval$selection_performance
+
+#### CONTINUE HERE
+# LASSO
+lasso_sel <- 
+
+
+################################################################################
+###   Function for: 
+###   Generating data
+###   Fitting lm() model
+###   Using model_var_performance() on variable selection method
+################################################################################
+
+
+# This function will specify the sample size and rhos, fit the lm() model, 
+# and will use the model_var_performance() function
+
+
+# Simulation function that will iterate N times
+simulation_sampsize_rho <- function(# Desired sample size
+                                    n = 250,
+                                    # Desired rho
+                                    rho = 0.35 ) {
+  # Simulate data
+  sim_dat <- gen_data(n = n,
+                      # Number of predictors
+                      p = n_vars, 
+                      # Non-zero predictors
+                      p1 = 5,
+                      # Vector of non-zero betas
+                      beta = betas,
+                      # Distribution
+                      family = "gaussian",
+                      # Correlation structure
+                      corr = "exchangeable",
+                      # Correlation coefficient
+                      rho = rho_vec[j])
+  
+  # Extract outcome for model
+  y <- sim_dat$y
+  # Extract predictors
+  X <- sim_dat$X
+  # Change colnames
+  colnames(X) <-  paste0("X", 1:n_vars)
+  
+  # Create data frame with outcome and predictors
+  model_dat <- data.frame(y = y, X)
+  # Change colnames of data frame
+  colnames(model_dat) <- c("y", paste0("X", 1:n_vars))
+  
+  # Get all variable names
+  vnames <- names(model_dat)
+  
+  # Fit regression model - exclude intercept
+  full_fit <- lm(as.formula(paste(vnames[1], '~', 
+                                  paste(vnames[2:(n_vars + 1)],
+                                        collapse = '+'))),
+                 data = model_dat)
+  
+  ### Backwards selection
+  backwards_sel <- ols_step_backward_p(full_fit) 
+  
+  ### LASSO
+  
+}
+
 
 # Run simulation
 sim_results <- sim1(N = 100)
@@ -293,6 +253,15 @@ sim_model_performance <- sim_results$model_performance
 # Variable selection performance
 sim_varsel_performance <- sim_results$selection_performance
 
+
+
+################################################################################
+###   Function for: 
+###   Iterating through the 6 different profiles
+###     - 3 rhos and 2 sample sizes
+################################################################################
+
+# 
 
 
 ############################## Checks/Playing around ##########################
@@ -339,6 +308,26 @@ fp <- sum(null_vars %in% selected_vars) # percentage
 ci <- confint.default(model_results, level = 1 - 0.05)
 # Coverage
 coverage <-  ci[, 1][-1] <= B & B <= ci[, 2][-1]
+
+
+# Tests with calculating coverage, etc.
+# Example: Vector for coverage with NA and TRUEs
+a <- data.frame(A = c(rep(NA, 5), rep(TRUE, 5)),
+                B = c(rep(TRUE, 5), rep(NA, 5)),
+                C = c(rep(NA, 3), rep(TRUE, 3), rep(NA, 4))
+                )
+# Taking the colMeans: 
+colMeans(a, na.rm = TRUE)
+# A B C 
+# 1 1 1 
+# Or do we want 0s and 1s
+b <- data.frame(A = c(rep(0, 5), rep(1, 5)),
+                B = c(rep(1, 5), rep(0, 5)),
+                C = c(rep(0, 3), rep(1, 3), rep(0, 4))
+)
+# Taking colMeans: 
+# A   B   C 
+# 0.5 0.5 0.3 
 
 # Checking correlations between Xs and y
 mean(cor(dat_list[[1]]$X)) # [1] 0.04970694
